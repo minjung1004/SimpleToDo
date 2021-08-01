07c7e2108080612be72cac44097e5c29a3902923
Min Jung <minjung10402@gmail.com>
2021-08-01 14:22:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.simpletodo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItem;
+    ItemsAdapter itemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItem = findViewById(R.id.rvItem);
+
+      //etItem.setText("I'm here java");
+        //can modify the etItem from here
+        
+        loadItems();
+
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
+            @Override
+            public void onItemLongClick(int position) {
+                //delete the item form the model
+                items.remove(position);
+                //notify the adapter
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(),"Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+        itemsAdapter = new ItemsAdapter(items,onLongClickListener);
+        rvItem.setAdapter(itemsAdapter);
+        rvItem.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String todoItem = etItem.getText().toString();
+                //add item to the model
+                items.add(todoItem);
+                //notify the adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+                //says item added when add item
+                Toast.makeText(getApplicationContext(),"Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
+    }
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+    //this function will loa items by reading every line of the data file
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(),Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading itmes", e);
+            items = new ArrayList<>();
+        }
+    }
+    //this function saves items by writing them into the data file]
+    //called only when add or remove item
+    private void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading itmes", e);
+        }
+
+    }
+}
